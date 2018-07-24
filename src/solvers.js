ea70cc54afa0bc8f575f871dc68f0fa8a3ff4b6c
Ryan Brennan <ryanbrennanbc1@gmail.com>
2018-07-24 16:26:55 -0700

    Rabbit Hole Version

--- src/solvers.js
+++ src/solvers.js
@@ -14,35 +14,63 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 
+// initialize boar
+//hasRowConflictAt: function (rowIndex)
+//hasColConflictAt: function (colIndex) {
+//togglePiece: function (rowIndex, colIndex) 
+// for (var row in board.attributes) {
+//   if (Array.isArray(board.attributes[row])) {
+//     board.attributes[row].forEach(function(square, column) {
 
-window.findNRooksSolution = function(n) {
+//       if (!board.hasRowConflictAt(row) &&
+//           !board.hasColConflictAt(column) &&
+//           !board.hasAnyRooksConflicts()) {
+
+//         board.togglePiece(row, column);
+
+//       }
+//     });
+//     solution.push(board.attributes[row]);
+//   }
+// }
+
+
+window.findNRooksSolution = function (n) {
   var solution = [];
-  var board = new Board({n: n}); //fixme // [ [0,0,0,0],[0,0,0,0] ]
-  // initialize boar
+  // initialize board
   //hasRowConflictAt: function (rowIndex)
   //hasColConflictAt: function (colIndex) {
   //togglePiece: function (rowIndex, colIndex) 
+  var board = new Board({ n: n }); //fixme // [ [0,0,0,0],[0,0,0,0] ]
+  
+  var recursiveRowTest = function (rowArray) {
+    for (var i = 0; i < rowArray.length; i++) {
+      // toggle a piece
+      board.togglePiece(row, i);
+      // test for conflicts
+      if (board.hasAnyRooksConflicts()) {
+        // if true (conflicts) then toggle again
+        board.togglePiece(row, i);
+        return rowArray;
+      }
+    }
+  };
+  
   for (var row in board.attributes) {
     if (Array.isArray(board.attributes[row])) {
-      board.attributes[row].forEach(function(square, column) {
-        if (!board.hasRowConflictAt(row) &&
-            !board.hasColConflictAt(column) &&
-            !board.hasAnyRooksConflicts()) {
-          
-          board.togglePiece(row, column);
-
-        }
-      });
+      console.log(board.attributes[row], 'before')
+      board.attributes[row] = recursiveRowTest(board.attributes[row]);
+      console.log(board.attributes[row], 'after')
       solution.push(board.attributes[row]);
     }
   }
-  
+
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
-window.countNRooksSolutions = function(n) {
+window.countNRooksSolutions = function (n) {
   var solutionCount = undefined; //fixme
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
@@ -50,7 +78,7 @@ window.countNRooksSolutions = function(n) {
 };
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
-window.findNQueensSolution = function(n) {
+window.findNQueensSolution = function (n) {
   var solution = undefined; //fixme
 
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
@@ -58,7 +86,7 @@ window.findNQueensSolution = function(n) {
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
-window.countNQueensSolutions = function(n) {
+window.countNQueensSolutions = function (n) {
   var solutionCount = undefined; //fixme
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
