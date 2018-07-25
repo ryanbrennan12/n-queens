9cd3ee4df0a692d0221d2e6718c132e3ef478d67
Ryan Brennan <ryanbrennanbc1@gmail.com>
2018-07-25 09:36:52 -0700

    Basic Minimum Requirements Met! -find countNQueens Solution using countNRooks Solution

--- src/solvers.js
+++ src/solvers.js
@@ -113,7 +113,28 @@ window.findNQueensSolution = function (n) {
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function (n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0; //fixme
+  var board = new Board({ n: n });
+  // function(combos)
+  var putRooks = function (combos) {
+    // if combos === n
+    if (combos === n) {
+      // increment solutionCount
+      solutionCount++;
+      return;
+    }
+    // for loop
+    for (var i = 0; i < n; i++) {
+      // togglepiece(combos, i)
+      board.togglePiece(combos, i);
+      // check anyROok conflicts
+      if (!board.hasAnyQueensConflicts()) {
+        putRooks(combos + 1);
+      }
+      board.togglePiece(combos, i);
+    }
+  };
+  putRooks(0);
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
